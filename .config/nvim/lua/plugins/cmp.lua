2e9d693c8c4aac648fb143db1b2e709efbe11c3a
Lucy McPhail <lucy.mcphail.p@gmail.com>
2024-05-06 19:59:30 +0100

    Remove border around cmp windows

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -10,10 +10,6 @@ local config = function()
         completion = {
             completeopt = 'menu,menuone,noinsert',
         },
-        window = {
-            completion = cmp.config.window.bordered(),
-            documentation = cmp.config.window.bordered(),
-        },
         mapping = cmp.mapping.preset.insert({
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
